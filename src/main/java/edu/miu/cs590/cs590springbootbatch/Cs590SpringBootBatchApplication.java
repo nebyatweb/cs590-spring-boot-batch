bd74ddfdcfb919b4be9a96459da3b03912288cd1
Nebyat Tibebu <nebyatweb@users.noreply.github.com>
2022-05-24 19:27:31 -0500

    add: new spring-boot project files

---
+++ src/main/java/edu/miu/cs590/cs590springbootbatch/Cs590SpringBootBatchApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs590.cs590springbootbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs590SpringBootBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs590SpringBootBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/miu/cs590/cs590springbootbatch/Cs590SpringBootBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs590.cs590springbootbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs590SpringBootBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
